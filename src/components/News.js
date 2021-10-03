1efdb885a472cb2dcdfa2239ae1837905fa2c78f
Weatherfore <90940004+Weatherfore@users.noreply.github.com>
2021-10-03 19:39:31 +0530

    Update News.js
    Commit

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../App.css';
+import {Link} from 'react-router-dom';
 async function searchNews(q) {
   q = encodeURIComponent(q);
   const response = await fetch(`https://bing-news-search1.p.rapidapi.com/news/search?freshness=Day&textFormat=Raw&safeSearch=Strict&q=${q}`, {
